9146dc25047aa713b0be54fef515e6d3afe1faab
Alfonso Saizar <alfo.saizar19@gmail.com>
2022-06-27 21:05:02 -0300

    Entrega 2.2

--- Entrega 2 - Manejo de archivos.js
+++ Entrega 2 - Manejo de archivos.js
@@ -1,35 +1,25 @@
-// >> Consigna: Implementar programa que contenga una clase llamada Contenedor que reciba el nombre del archivo con el que va a trabajar e implemente los siguientes métodos:
-
-// save(Object): Number - Recibe un objeto, lo guarda en el archivo, devuelve el id asignado.
-// getById(Number): Object - Recibe un id y devuelve el objeto con ese id, o null si no está.
-// getAll(): Object[] - Devuelve un array con los objetos presentes en el archivo.
-// deleteById(Number): void - Elimina del archivo el objeto con el id buscado.
-// deleteAll(): void - Elimina todos los objetos presentes en el archivo.
-
 const fs = require('fs');
 
 class Contenedor{
     constructor(archivo){
         this.archivo = archivo;
     }
-    static id = 0;
     static productsList = [];
     
     save(title,price){
-        Contenedor.id++;
-        fs.appendFile('./productos.txt',`ID: ${Contenedor.id}\nTitle: ${title}\nPrice: ${price}\n\n`,error =>{
+        Contenedor.productsList.push({'ID': Contenedor.productsList.length + 1, 'Title': title, 'Price': price})
+        fs.writeFileSync('./productos.txt',JSON.stringify(Contenedor.productsList) ,error =>{
             if (error) {
                 console.log('Se ha producido un error al guardar el producto')
             } else {
                 console.log('Guardado con exito!')
             }
         })
-        Contenedor.productsList.push(title)
-        return Contenedor.id;
+        return Contenedor.productsList.length + 1;
     }
 
     getById(number){
-        if (number-1 <= Contenedor.id){
+        if (number-1 <= Contenedor.productsList.length){
             console.log(`Id: ${number} -`,Contenedor.productsList[number-1])
         } else {
             console.log('Id: Null');
@@ -38,12 +28,23 @@ class Contenedor{
 
     getAll(){
         for (let i = 1; i <= Contenedor.productsList.length;i++){
-            console.log(`ID: ${i} - ${Contenedor.productsList[i-1]}`)
+            console.log(Contenedor.productsList[i-1])
         }
     }
 
     deleteById(number){
-        
+        if (number-1 <= Contenedor.productsList.length){
+            Contenedor.productsList.splice(number-1,number -1);
+            fs.writeFileSync('./productos.txt',JSON.stringify(Contenedor.productsList) ,error =>{
+            if (error) {
+                console.log('Se ha producido un error al borrar el producto')
+            } else {
+                console.log(`Producto ${number} borrado con exito!`)
+            }
+            })
+        } else{
+            console.log('No hay producto para borrar.')
+        }
     }
 
     deleteAll(){
@@ -64,11 +65,10 @@ class Contenedor{
 const productos = new Contenedor('Productos.txt');
 console.log(productos.save('Licuadora',3));
 console.log(productos.save('Teclado',124));
-console.log(productos.getAll())
 console.log(productos.save('Celular',34));
-console.log(productos.getAll())
 console.log(productos.getById(2));
 console.log(productos.getById(5));
-console.log(productos.deleteAll());
-console.log(Contenedor.productsList)
-console.log(productos.getAll())
+console.log(productos.deleteById(2));
+// console.log(productos.deleteAll());
+console.log(Contenedor.productsList);
+console.log(productos.getAll());
